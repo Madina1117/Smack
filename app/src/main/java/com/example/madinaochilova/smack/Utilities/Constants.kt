994781011efc8250c74f7580b64d30f42c2cb0d8
Madina Ochilova <ochilova.madina@gmail.com>
2018-11-29 16:20:17 -0800

    lesson 11

--- app/src/main/java/com/example/madinaochilova/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/madinaochilova/smack/Utilities/Constants.kt
@@ -3,4 +3,5 @@ package com.example.madinaochilova.smack.Utilities
 const val BASE_URL = "https://chatchh.herokuapp.com/v1/"
 //const val BASE_URL = "https://10.0.2.2:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
 
